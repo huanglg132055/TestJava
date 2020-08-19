e019197e629bb7a4d120a931c702a8219448921e
huanglg <huangliguo123>
2020-08-20 00:17:16 +0800

    T-3-饿汉式单例模式

---
+++ src/main/java/designPatterns/sigleton/SingletonDemo01.java
@@ -0,0 +1,20 @@
+package designPatterns.sigleton;
+
+/**
+ * 饿汉式单例模式
+ * @ClassName SingletonDemo01
+ * @Description: TODO
+ * @Author huanglg
+ * @Date 2020/8/19
+ * @Version V1.0
+ **/
+public class SingletonDemo01 {
+
+    private static final SingletonDemo01 instance = new SingletonDemo01();
+
+    private SingletonDemo01(){}
+
+    public static SingletonDemo01 getInstance(){
+        return instance;
+    }
+}
