43df7deeb2e32aade5747547179dc634a687e123
admin <15611710327@163.com>
2021-01-24 23:30:38 +0800

    T-行为模式（解释器模式和访问者模式）

---
+++ src/main/java/designPatterns/actionModel/interpreter/MyInterpreter.java
@@ -0,0 +1,10 @@
+package designPatterns.actionModel.interpreter;
+
+/**
+ * 解释器模式
+ * EL表达式
+ * 正则表达式
+ * 数学表达式
+ */
+public class MyInterpreter {
+}
---
+++ src/main/java/designPatterns/actionModel/visitor/MyVisitor.java
@@ -0,0 +1,10 @@
+package designPatterns.actionModel.visitor;
+
+/**
+ * 访问者模式
+ * xml文档解释器设计
+ * 编译器的设计
+ * 复杂集合对象的处理
+ */
+public class MyVisitor {
+}
